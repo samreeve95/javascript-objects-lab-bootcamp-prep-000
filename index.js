ae552dc578ea4ee9f25ee0f6dd7089cf27998d1a
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 05:30:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ var recipes = {"key": "value"};
 updateObjectWithKeyAndValue(object, key, value)
 
 destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+
+destructivelyUpdateObjectWithKeyAndValue(object, key, value)
